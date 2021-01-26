c41e5c11cfdd95756122fabce2a9d670c676ae00
Matheus Souza <matheus.ramirez019@gmail.com>
2021-01-26 11:50:04 -0300

    final aula 01

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
-
+import db from '../db.json';
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
@@ -8,17 +8,14 @@ const GlobalStyle = createGlobalStyle`
   }
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
+     
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
